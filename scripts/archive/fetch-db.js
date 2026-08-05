9a0d0202c903aac3428893ed69ae880837f99175
Iman Prasetyo <imaniendra@gmail.com>
2026-08-05 11:23:30 +0800

    Clean up old development scripts from archive

--- scripts/archive/fetch-db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri).then(async () => {
-  const PaketMatkul = mongoose.model('PaketMatkul', new mongoose.Schema({}, { strict: false, collection: 'paketmatkuls' }));
-  const pakets = await PaketMatkul.find().lean();
-  console.log(JSON.stringify(pakets[0], null, 2));
-  process.exit(0);
-}).catch(console.error);
--- scripts/archive/temp.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI);
-const schema = new mongoose.Schema({ foto_kantor_desa: String, foto_kantor_bumdes: String }, { collection: 'mitrakkls' });
-const Model = mongoose.model('MitraKKL', schema);
-const data = await Model.find({}, { foto_kantor_desa: 1, foto_kantor_bumdes: 1 });
-console.log(JSON.stringify(data, null, 2));
-process.exit(0);
